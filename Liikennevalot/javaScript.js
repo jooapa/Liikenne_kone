871ab5b6d8035cd37a3330952dcf29d6e9d1fc2b
jooapas <jooa.akonpelto@gmail.com>
2022-11-11 10:36:59 +0200

    progress

--- Liikennevalot/javaScript.js
+++ Liikennevalot/javaScript.js
@@ -17,52 +17,171 @@ const sleep = (time) => {
 	var car2id = null;
 	var car3id = null;
 	var car4id = null;
+	lightSpot1 = 0;
+	lightSpot2 = 0;
+	lightSpot3 = 0;
+	lightSpot4 = 0;
+
+	var FirstTimeGOingThrough1 = 1
+	var FirstTimeGOingThrough2 = 1
+	var FirstTimeGOingThrough3 = 1
+	var FirstTimeGOingThrough4 = 1
+
 
 
 	function change(){
 
 		var elem = document.getElementById("myButton1");
-			if (elem.value=="Punainen"){
-				elem.value = "Vihreä";
-			}
-			else{
-				elem.value = "Punainen";
-				imgContainer.appendChild(sourceImage.cloneNode(true));
-			} 
+		if (elem.value=="Punainen"){
+			elem.value = "Vihreä";
+		}
+		else{
+			elem.value = "Punainen";
+			imgContainer.appendChild(sourceImage.cloneNode(true));
+		} 
 	}	
 	
 	function Start(){
-		lightSpot = 2;
-		var imgLight = document.createElement('img');
-		imgLight.src = "lightc.png";
-		imgLight.style.width = "auto";
-		imgLight.style.height = "50px";
-		imgLight.style.position = "absolute";
-		imgLight.style.filter = "saturate(1)";
+		
+		var imgLight1 = document.createElement('img');
+		imgLight1.src = "lightc.png";
+		imgLight1.style.transform = "translate(260px, 280px)";
+		imgLight1.style.transform += "rotate(90deg)";
+		imgLight1.style.width = "auto";
+		imgLight1.style.height = "50px";
+		imgLight1.style.position = "absolute";
+		var olddata1=document.getElementById("TrafficLight1").lastChild;
 
-		if(lightSpot == 0){
-			imgLight.style.transform = "translate(245px, 280px)";
-			imgLight.style.transform += "rotate(90deg)";
-			imgLight.style.filter += "hue-rotate(290deg)";
-			document.getElementById('myContainer').appendChild(imgLight);
-		}	
+		var imgLight2 = document.createElement('img');
+		imgLight2.src = "lightc.png";
+		imgLight2.style.transform = "translate(280px, 105px)";
+		imgLight2.style.transform += "rotate(0deg)";
+		imgLight2.style.width = "auto";
+		imgLight2.style.height = "50px";
+		imgLight2.style.position = "absolute";
+		var olddata2=document.getElementById("TrafficLight2").lastChild;
 
-		if(lightSpot == 1){
-			imgLight.style.transform = "translate(245px, 280px)";
-			imgLight.style.transform += "rotate(90deg)";
-			imgLight.style.filter += "hue-rotate(0deg)";
-			document.getElementById('myContainer').appendChild(imgLight);
-		}	
+		var imgLight3 = document.createElement('img');
+		imgLight3.src = "lightc.png";
+		imgLight3.style.transform = "translate(105px, 90px)";
+		imgLight3.style.transform += "rotate(-90deg)";
+		imgLight3.style.width = "auto";
+		imgLight3.style.height = "50px";
+		imgLight3.style.position = "absolute";
+		var olddata3=document.getElementById("TrafficLight3").lastChild;
 
-		if(lightSpot == 2){
-			imgLight.style.transform = "translate(245px, 280px)";
-			imgLight.style.transform += "rotate(90deg)";
-			imgLight.style.filter += "hue-rotate(30deg)";
-			document.getElementById('myContainer').appendChild(imgLight);
-		}	
+		var imgLight4 = document.createElement('img');
+		imgLight4.src = "lightc.png";
+		imgLight4.style.transform = "translate(85px, 260px)";
+		imgLight4.style.transform += "rotate(180deg)";
+		imgLight4.style.width = "auto";
+		imgLight4.style.height = "50px";
+		imgLight4.style.position = "absolute";
+		var olddata4=document.getElementById("TrafficLight4").lastChild;
+
+
+
+		if(lightSpot1 == 0){
+			if(FirstTimeGOingThrough1 == 0){FirstTimeGOingThrough1 = 1
+				document.getElementById("TrafficLight1").removeChild(olddata1);
+			}
+			imgLight1.style.filter += "hue-rotate(30deg)";
+			document.getElementById('TrafficLight1').appendChild(imgLight1);
+		}		
+		if(lightSpot1 == 1){
+			imgLight1.style.filter += "hue-rotate(290deg)";
+			document.getElementById("TrafficLight1").removeChild(olddata1);
+			document.getElementById('TrafficLight1').appendChild(imgLight1);
+		}
+		
+		if(lightSpot2 == 0){
+			imgLight2.style.filter += "hue-rotate(30deg)";
+			if(FirstTimeGOingThrough2 == 0){FirstTimeGOingThrough2 = 1
+				document.getElementById("TrafficLight2").removeChild(olddata2);
+			}
+			document.getElementById('TrafficLight2').appendChild(imgLight2);
+		}		
+
+		if(lightSpot2 == 1){
+			imgLight2.style.filter += "hue-rotate(290deg)";
+			if(FirstTimeGOingThrough2 == 0){FirstTimeGOingThrough2 = 1
+				document.getElementById("TrafficLight2").removeChild(olddata2);
+			}			
+			document.getElementById('TrafficLight2').appendChild(imgLight2);
+		}
+		
+		if(lightSpot3 == 0){
+			imgLight3.style.filter += "hue-rotate(30deg)";
+			if(FirstTimeGOingThrough3 == 0){FirstTimeGOingThrough3 = 1
+				document.getElementById("TrafficLight3").removeChild(olddata3);
+			}	
+			document.getElementById('TrafficLight3').appendChild(imgLight3);
+		}		
+
+		if(lightSpot3 == 1){
+			imgLight3.style.filter += "hue-rotate(290deg)";
+			if(FirstTimeGOingThrough3 == 0){FirstTimeGOingThrough3 = 1
+				document.getElementById("TrafficLight3").removeChild(olddata3);
+			}	
+			document.getElementById('TrafficLight3').appendChild(imgLight3);
+			console.log("31");
+		}
+		if(lightSpot4 == 0){
+			imgLight4.style.filter += "hue-rotate(30deg)";
+			if(FirstTimeGOingThrough4 == 0){FirstTimeGOingThrough4 = 1
+				document.getElementById("TrafficLight4").removeChild(olddata4);
+			}	
+			document.getElementById('TrafficLight4').appendChild(imgLight4);console.log("32");
+		}		
+
+		if(lightSpot4 == 1){
+			imgLight4.style.filter += "hue-rotate(290deg)";
+			if(FirstTimeGOingThrough4 == 0){FirstTimeGOingThrough4 = 1
+				document.getElementById("TrafficLight4").removeChild(olddata4);
+			}	
+			document.getElementById('TrafficLight4').appendChild(imgLight4)
+		}
+		
+		FirstTimeGOingThrough1 = 0
+		FirstTimeGOingThrough2 = 0
+		FirstTimeGOingThrough3 = 0
+		FirstTimeGOingThrough4 = 0
+		
 	}
-
-
+	
+	function Start11(){
+		 
+		lightSpot1 = 0;
+		Start();
+	}
+	function Start12(){
+		lightSpot1 = 1;
+		Start();
+	}
+	function Start21(){
+		lightSpot2 = 0;
+		Start();
+	}
+	function Start22(){
+		lightSpot2 = 1;
+		Start();
+	}
+	function Start31(){
+		lightSpot3 = 0;
+		Start();
+	}
+	function Start32(){
+		lightSpot3 = 1;
+		Start();
+	}
+	function Start41(){
+		lightSpot4 = 0;
+		Start();
+	}
+	function Start42(){
+		lightSpot4 = 1;
+		Start();
+	}
 
 
 
@@ -78,7 +197,7 @@ const sleep = (time) => {
 
 		console.log(rng)
 		if(rng == 0 && car1Exist == false){let pos = 0;car1Exist = true;
-			img.style.transform = "translate(210px, 400px)";
+			img.style.transform = "translate(220px, 400px)";
 			document.getElementById('myContainer').appendChild(img);
 			
 			
@@ -93,7 +212,7 @@ const sleep = (time) => {
 		}
 
 		if(rng == 1 && car2Exist == false){let pos = 0;car2Exist = true;
-			img.style.transform = "translate(410px, 150px)";
+			img.style.transform = "translate(420px, 150px)";
 			img.style.transform += "rotate(-90deg)";
 			document.getElementById('myContainer').appendChild(img);
 			
@@ -110,7 +229,7 @@ const sleep = (time) => {
 		}
 		
 		if(rng == 2 && car3Exist == false){let pos = 0;car3Exist = true;
-			img.style.transform = "translate(150px, -30px)";
+			img.style.transform = "translate(160px, -30px)";
 			img.style.transform += "rotate(180deg)";
 			document.getElementById('myContainer').appendChild(img);
 			
@@ -145,7 +264,7 @@ const sleep = (time) => {
 	} 
 	
 	
-	
+
 
 
 
